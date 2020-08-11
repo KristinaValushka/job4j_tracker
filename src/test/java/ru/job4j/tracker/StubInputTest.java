0db519c1bbe5ff38ca1f38355ac62f5acec800a8
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-10 18:21:31 -0700

    4.2. Статические методы. [#309941]

---
+++ src/test/java/ru/job4j/tracker/StubInputTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import junit.framework.TestCase;
+
+public class StubInputTest extends TestCase {
+
+    public void testAskStr() {
+    }
+
+    public void testAskInt() {
+    }
+}
