99e0408334b5782e25e3a1bdc3935139a4872da5
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-11 19:52:10 -0700

    5.1. Тестирование. Подготовка данных. [#309943]

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class FindByNameItem implements UserAction{
+public class FindByNameAction implements UserAction{
     @Override
     public String name() {
         return "=== Found item by name";
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -82,7 +82,14 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction()
+                new CreateAction(),
+                new SearchAllAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindByIdAction(),
+                new FindByNameAction(),
+                new ExitAction()
+
         };
         new StartUI().init(input, tracker, actions);
         }
