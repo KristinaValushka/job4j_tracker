7b471acccf03e5a8441a006886b10e1a81cdc436
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-12 20:25:11 -0700

    9. Написать тесты на StartUI. [#309946]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -60,6 +60,12 @@ public class StartUI {
         }
     }
 
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
+
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
@@ -78,10 +84,11 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
+                new CreateAction(output),
                 new SearchAllAction(),
                 new ReplaceAction(),
                 new DeleteAction(),
@@ -90,7 +97,7 @@ public class StartUI {
                 new ExitAction()
 
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
         }
     }
 
